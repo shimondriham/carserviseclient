69173b05b83480beed67e1c4efc294cfd16fd67f
shimon driham <95618903+shimondriham@users.noreply.github.com>
2022-11-15 23:53:25 +0200

    start

--- src/comps/general_comps/clientFooter.js
+++ src/comps/general_comps/clientFooter.js
@@ -4,7 +4,7 @@ function ClientFooter(props){
   return(
     <footer className='footer-client container-fluid bg-dark text-white fixed-bottom'>
       <div className="container text-center">
-      ⓒ All rights reserved to Shimon Driham and Uriya Davidi.
+      ⓒ All rights reserved to Shimon Driham .
       </div>
       
       </footer> 
